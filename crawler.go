70791d4ad6a4072cdead921f776f49d48b7d2b1f
Chris <chrise@moo.com>
2017-11-03 08:04:08 +0000

    This works with a buffered channel.

--- crawler.go
+++ crawler.go
@@ -25,12 +25,11 @@ func NewCrawler(options ...func(*Crawler)) *Crawler {
 func (c *Crawler) run(page_url string, max_depth int) []string {
 	c.max_depth = max_depth
 
-	urls := make(chan string)
+	urls := make(chan string, 10000)
 	var wg sync.WaitGroup
 
 	wg.Add(1)
 	go c.crawl(page_url, 0, urls, &wg)
-	wg.Done()
 
 	//
 	//// extract the keys and sort
@@ -39,27 +38,28 @@ func (c *Crawler) run(page_url string, max_depth int) []string {
 	//	keys = append(keys, k)
 	//}
 	//sort.Strings(keys)
-
 	fmt.Println("Waiting for workers to finish")
 	wg.Wait()
 	fmt.Println("Done waiting")
-
-	for elem := range urls {
-		fmt.Println(elem)
-	}
 
 	fmt.Println("Closing Channel")
 	close(urls)
 	fmt.Println("Channel Closed")
 
+	for elem := range urls {
+		fmt.Println(elem)
+	}
+
+
+
 	return []string{"hello"}
 }
 
 func (c *Crawler) crawl(page_url string, depth int, urls chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
 
 	// return if we've reached max depth
 	if depth >= c.max_depth {
-		wg.Done()
 		return
 	}
 
